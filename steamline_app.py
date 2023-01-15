c7ebd9a9e2d99ac3815afaab4c7f4421a1154465
muniyanmurugan10 <122740995+muniyanmurugan10@users.noreply.github.com>
2023-01-15 23:33:32 +0530

    Update steamline_app.py

--- steamline_app.py
+++ steamline_app.py
@@ -2,9 +2,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner.')
 
 streamlit.header('Break fast Favourites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinack & rocket smoothie')
-streamlit.text('Hard boiled free range egg')
-streamlit.text('Avacode Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale, Spinack & rocket smoothie')
+streamlit.text('🐔 Hard boiled free range egg')
+streamlit.text('🥑🍞 Avacode Toast')
 
 
