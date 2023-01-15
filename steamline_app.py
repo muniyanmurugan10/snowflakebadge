8657c917ac219a6ac68eac920c8907bd45e75a82
muniyanmurugan10 <122740995+muniyanmurugan10@users.noreply.github.com>
2023-01-15 23:17:27 +0530

    Update steamline_app.py

--- steamline_app.py
+++ steamline_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('i am learning snowflake and streamlit')
+
+streamlit.header('starting badge 2')
+streamlit.text('Pongal festival')
+streamlit.text('Mattu pongal')
+streamlit.text('Canum pongal')
